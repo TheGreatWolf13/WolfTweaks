914a72425a4b004d0a6dde1619a5c609d4509524
TheGreatWolf <16447647+thegreatwolf13@users.noreply.github.com>
2025-07-19 16:18:49 -0300

    Fixed Game Rules Integer edit box getting unfocused incorrectly.

--- src/main/java/tgw/wolf_tweaks/mixin/MixinContainerEventHandler.java
+++ src/main/java/tgw/wolf_tweaks/mixin/MixinContainerEventHandler.java
@@ -3,6 +3,7 @@ package tgw.wolf_tweaks.mixin;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.events.ContainerEventHandler;
 import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.worldselection.EditGameRulesScreen;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,7 +20,7 @@ public interface MixinContainerEventHandler extends GuiEventListener {
     @Inject(method = "mouseReleased", at = @At(value = "RETURN", ordinal = 0))
     default void onMouseReleased(double d, double e, int i, CallbackInfoReturnable<Boolean> cir) {
         GuiEventListener focused = this.getFocused();
-        if (!(focused instanceof EditBox)) {
+        if (!(focused instanceof EditBox) && !(focused instanceof EditGameRulesScreen.IntegerRuleEntry)) {
             this.setFocused(null);
         }
     }
